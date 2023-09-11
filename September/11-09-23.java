4e4b8d100b18a5fa749485c8e227b2d2ce290c73
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2023-09-11 21:49:20 +0530

    Lucky number  11-09-23.java solution

---
+++ September/11-09-23.java
@@ -0,0 +1,13 @@
+class Solution{
+public static boolean isLucky(int n)
+{
+        if (n % 2 == 0) return false;
+        int x = 2;
+        while (n >= x) {
+            if (n % x == 0) return false;
+            n -= n / x;
+            x++;
+        }
+        return true;      
+    }
+}
