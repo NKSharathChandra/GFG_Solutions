5ca74ca9511ed2e93b6437036c4fb5160671bf52
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2023-09-18 21:03:36 +0530

    Power of 2 18-09-23.java solution

---
+++ September/18-09-23.java
@@ -0,0 +1,13 @@
+class Solution{
+    public static boolean isPowerofTwo(long n){
+        if( n == 0 ) return false;
+        if( n == 1 ) return true;
+        
+        while( n > 1 ){
+            if( n % 2 != 0 ) return false;
+            n /= 2;
+        }
+        return true;
+    }
+    
+}
