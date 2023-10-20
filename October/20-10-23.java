b9e38f2676563c4570b96f14862036be8b7ecf37
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2023-10-20 21:52:57 +0530

    Form a number divisible by 3 using array digits 20-10-23.java solution

---
+++ October/20-10-23.java
@@ -0,0 +1,15 @@
+class Solution {
+    static int isPossible(int N, int arr[]) {
+        int digit, sum=0;
+        for(int i=0;i<N;i++){
+           int num=arr[i];
+           while(num>0){
+              digit=num%10;
+              sum=sum+digit;
+              num=num/10;
+           }
+           
+        }
+        return sum%3 == 0?1:0;
+    }
+}
