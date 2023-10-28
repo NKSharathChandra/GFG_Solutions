b33c1aa785ff846b9261f44360895491d85eeb16
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2023-10-28 22:58:34 +0530

    Bleak Numbers 28-10-23.java solution

---
+++ October/28-10-23.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    public int is_bleak(int n)
+    {
+       int m=n;
+      
+       
+       for(int i=m-1; i>=m-32; i--){
+           
+           if(i+countSetBits(i) == m) {
+              return 0;
+           }
+           
+       }
+       
+       return 1;
+    }
+    
+    int countSetBits(int n){
+        int setBits = 0;
+        
+        while(n>0){
+            if((n&1) == 1) setBits++;
+            n=n/2;
+        }
+        return setBits;
+    }
+}
+
