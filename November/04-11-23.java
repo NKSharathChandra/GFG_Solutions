ca65f584412d539e784ee8722d2b5542d2447c80
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2023-11-05 20:40:02 +0530

    Find transition point 04-11-23.java solution

---
+++ November/04-11-23.java
@@ -0,0 +1,17 @@
+class Solution {
+int transitionPoint(int arr[], int n) {
+        
+        int start = 0;
+        int end = n-1;
+        int ans = -1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] == 1){
+                ans = mid;
+                end = mid-1;
+            }
+            else start = mid+1;
+        }
+        return ans;
+    }
+}
