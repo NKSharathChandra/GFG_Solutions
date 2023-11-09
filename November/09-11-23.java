d2030a1c0264442c59df80dba5e94c90e8676ca0
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2023-11-09 22:34:45 +0530

    Predict the column 09-11-23.java solution

---
+++ November/09-11-23.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    int columnWithMaxZeros(int arr[][], int n)
+    {
+        // code here
+        int max=0;
+        int ans=0;
+        for(int i=0; i<n; i++){
+            int count=0;
+            for(int j=0; j<n; j++){
+                if(arr[j][i]==0){
+                    count++;
+                }
+            }
+            if(count>max){
+                max=count;
+                ans=i;
+            }
+        }
+        if(max==0){
+            return -1;
+        }
+        return ans;
+    }
+}
