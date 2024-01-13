fbef210175af306c76e623850861e9eaebfcccb8
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2024-01-13 23:09:19 +0530

    Insertion Sort for Singly Linked List 13-01-24.java solution

---
+++ 2024/January/13-01-24.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public static Node insertionSort(Node head)
+    {
+        //code here
+        Node dummy = head;
+        
+        for(Node i = head; i != null; i = i.next){
+            for(Node j = i.next; j != null; j = j.next){
+                if(i.data > j.data){
+                    int temp = i.data;
+                    i.data = j.data;
+                    j.data = temp;
+                }
+            }
+        }
+        return dummy;
+    }
+}
