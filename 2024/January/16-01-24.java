d4c2af7a14e89210c7196ef16550cafb1ddbe856
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2024-01-16 23:36:35 +0530

    Sequence of Sequence 16-01-24.java solution

---
+++ 2024/January/16-01-24.java
@@ -0,0 +1,8 @@
+class Solution{
+    static int numberSequence(int m, int n)
+    {
+        if(m < n)return 0;
+        if(n == 0)return 1;
+        return numberSequence(m/2,n-1)+numberSequence(m-1,n);
+    }
+}
