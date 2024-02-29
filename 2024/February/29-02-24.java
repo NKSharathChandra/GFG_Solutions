a3241124ec21b25ea2a0f601a0b9b60cec3044ee
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2024-02-29 23:55:59 +0530

    Sum of bit differences 29-02-24.java solution

---
+++ 2024/February/29-02-24.java
@@ -0,0 +1,13 @@
+class Solution {
+    long sumBitDifferences(int[] arr, int n) {
+        long cnt = 0;
+        for(int i=31;i>=0;i--){
+           long a = 0;
+           for(int j=0;j<n;j++){
+               a+=(arr[j]>>i)&1;
+           }
+           cnt += 2*a*(n-a);
+        }
+        return cnt;
+    }
+}
