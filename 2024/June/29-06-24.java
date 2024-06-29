adb7f3c39f764052ff4ee92ce704a9d958a48bd3
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2024-06-29 22:24:12 +0530

    Identical Linked Lists 29-06-24.java solution

---
+++ 2024/June/29-06-24.java
@@ -0,0 +1,15 @@
+class Solution {
+    // Function to check whether two linked lists are identical or not.
+    public boolean areIdentical(Node head1, Node head2) {
+        if(head1 == null || head2 == null){
+            if(head1 != head2) return false;
+            else return true;
+        }
+        
+        if(head1.data == head2.data){
+            return areIdentical(head1.next, head2.next);
+        } else {
+            return false;
+        }
+    }
+}
