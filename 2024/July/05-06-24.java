769504432135d6079f7d20f68ac4d24de17fad2b
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2024-07-05 21:21:49 +0530

    Vertical Width of a Binary Tree 05-06-24.java solution

---
+++ 2024/July/05-06-24.java
@@ -0,0 +1,18 @@
+class Solution {
+static int mn=0,mx=0;
+    void help(Node root,int pos){
+        if(root==null)return;
+        mn=Math.min(mn,pos);
+        mx=Math.max(mx,pos);
+        help(root.left,pos-1);
+        help(root.right,pos+1);
+    }
+    public int verticalWidth(Node root) {
+        // code here.
+        if(root==null)return 0;
+        mn=0;
+        mx=0;
+        help(root,0);
+        return mx+Math.abs(mn)+1;
+    }
+}
