84c29c16a70f5eac559ceab35180e4ddb46834b9
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2024-07-07 00:00:42 +0530

    Populate Inorder Successor for all nodes 07-07-24.java solution

---
+++ 2024/July/07-07-24.java
@@ -0,0 +1,14 @@
+class Solution {
+    Node next = null;
+    public void populateNext(Node root) {
+        // code here
+        if(root==null){
+            return;
+        }
+        populateNext(root.right);
+        root.next = next;
+        next = root;
+        populateNext(root.left);
+    }
+}
+
