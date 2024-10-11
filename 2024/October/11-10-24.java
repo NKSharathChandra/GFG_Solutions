8efc92f3d841d33beef959a87e726e840b85992b
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2024-10-11 23:10:31 +0530

    Reorganize The Array 11-10-24.java solution

---
+++ 2024/October/11-10-24.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> rearrange(List<Integer> arr) {
+        
+        for(int i=0; i<arr.size(); i++){
+            while(arr.get(i)!=-1 && arr.get(i)!=i){
+                int correctIndex = arr.get(i);
+                
+                if(arr.get(correctIndex) == correctIndex){
+                    break;
+                }
+                
+                int temp = arr.get(i);
+                arr.set(i, arr.get(correctIndex));
+                arr.set(correctIndex, temp);
+            }
+        }
+        
+        for(int i=0; i<arr.size(); i++){
+            if(arr.get(i)!=i){
+                arr.set(i, -1);
+            }
+        }
+        
+        return arr;
+    }
+}
