6f1c519a8684d6bac2476563a0a75ca7f3915152
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2024-12-11 23:30:42 +0530

    Merge Without Extra Space 11-12-24.java solution

---
+++ 2024/December/11-12-24.java
@@ -0,0 +1,24 @@
+class Solution{
+  public void mergeArrays(int a[], int b[]) {
+        // code here
+        int left = a.length-1;
+        int right = 0;
+        while(left>=0 && right<b.length)
+        {
+            if(a[left]>b[right])
+            {
+                int temp=a[left];
+                a[left]=b[right];
+                b[right]=temp;
+                left--;
+                right++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+    }
+}
